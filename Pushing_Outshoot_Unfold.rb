31073e8fea07a47c94fd69b85542c39f19282afd
Cole, Harshini, Cassidy <po-unfold@colewilson.xyz>
2021-05-13 18:42:38 +0000

    Added days, and ASCII, and a whole bunch of other stuff

--- Pushing_Outshoot_Unfold.rb
+++ Pushing_Outshoot_Unfold.rb
@@ -1,14 +1,14 @@
 # Created with sailboat, the Python releaser
 
-# v0.0.2-rc1
+# v0.0.3
 
 class Pushing_Outshoot_Unfold < Formula
   include Language::Python::Virtualenv
 
   desc "A game about a teen getting through quarantine in Covid."
   homepage "https://github.com/po-unfold"
-  url "https://files.pythonhosted.org/packages/e3/4b/83cee12cd62c893ec9e0a3ce3d38e6414366c482828280acd6940809d680/pushing_outshoot_unfold-0.0.1.tar.gz" # These lines must be configured during release, not build.
-  sha256 "8a4fff9cae379e563f5f3119209bc096e38b5a9f6679fd4c84167cc55a01b984" # ^^^
+  url "https://files.pythonhosted.org/packages/05/0e/7debb645b0762e6a7e389f2e793c98956888b708b75707d8c8db3ccd1e77/pushing_outshoot_unfold-0.0.3.tar.gz" # These lines must be configured during release, not build.
+  sha256 "ff438de62184c065e6fab7cd0103c99d5232234dfbecdc5c9aefd13c9faeb2ba" # ^^^
   license "none"
 
   livecheck do
@@ -20,6 +20,10 @@ class Pushing_Outshoot_Unfold < Formula
    resource "blessed" do
       url "https://files.pythonhosted.org/packages/68/c7/5f21c1b0e7f343bad166c3139e594639cd54e9b97db7760dff035b35065e/blessed-1.18.0.tar.gz"
       sha256 "1312879f971330a1b7f2c6341f2ae7e2cbac244bfc9d0ecfbbecd4b0293bc755"
+   end
+   resource "toml" do
+      url "https://files.pythonhosted.org/packages/be/ba/1f744cdc819428fc6b5084ec34d9b30660f6f9daaf70eead706e3203ec3c/toml-0.10.2.tar.gz"
+      sha256 "b3bda1d108d5dd99f4a20d24d9c348e91c4db7ab1b749200bded2f839ccbe68f"
    end
 
 
